7651c11add36b8b01835675ba863e8f9fdcc4fb9
Matheus Costa <matheus.design12@gmail.com>
2022-07-23 14:10:07 -0300

    Create Generic Class - Singleton

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+
+namespace Maze.Enums
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private Singleton(){}
+
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                lock (_instance)
+                {
+                    Initialize();
+                    return _instance;
+                }
+            }
+        }
+
+        [RuntimeInitializeOnLoadMethod]
+        private static void Initialize()
+        {
+            if (IsNull())
+            {
+                _instance = FindObjectOfType<T>();
+                
+                if (IsNull())
+                {
+                    GameObject singletonObject = new GameObject(typeof(T) + " (Singleton)");
+                    singletonObject.hideFlags = HideFlags.NotEditable;
+                    
+                    _instance = singletonObject.AddComponent<T>();
+                    
+                    DontDestroyOnLoad(singletonObject);
+                }
+                
+            }
+        }
+
+        private void Awake()
+        {
+            if (!IsNull())
+            {
+                DontDestroyOnLoad(this);
+            }
+        }
+
+        private static bool IsNull()
+        {
+            lock (_instance)
+            {
+                return _instance == null && _instance.Equals(null);
+            }
+        }
+    }
+}
