714eeb816fe5cbb4e3b5e2103382373bee698d08
Matheus Costa <matheus.design12@gmail.com>
2022-07-23 14:27:03 -0300

    Menu UI Component

---
+++ Assets/Scripts/UI/MenuUI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Maze.Enums;
+using Maze.Managers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Maze.UI
+{
+    [DisallowMultipleComponent]
+    public class MenuUI : MonoBehaviour
+    {
+        [Header("Buttons")] 
+        public Button btn_play;
+        public Button btn_exit;
+
+        private void Awake()
+        {
+            InitializeButtons();
+        }
+
+        private void InitializeButtons()
+        {
+            btn_play.onClick.AddListener(() => SceneController.Instance.LoadAsyncScene(SceneIndex.Gameplay));
+            btn_exit.onClick.AddListener(Application.Quit);
+        }
+    }
+}
