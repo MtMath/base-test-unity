4411964eff4229da6d2d1d6f98824781f3d69a6b
Matheus Costa <matheus.design12@gmail.com>
2022-07-24 19:28:53 -0300

    Flash Lighr Behaviour

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Maze
+{
+    [RequireComponent(typeof(Light))]
+    public class Flashlight : MonoBehaviour
+    {
+
+        private Light pointLight;
+
+        public void TurnOn()
+        {
+            pointLight.enabled = true;
+        }
+
+        public void TurnOff()
+        {
+            pointLight.enabled = false;
+        }
+
+    }
+}
