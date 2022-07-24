b4bbb6f1116a9c71e198c3b427fb04ba5f7c6689
Matheus Costa <matheus.design12@gmail.com>
2022-07-24 15:17:26 -0300

    Character Properties SO

---
+++ Assets/Scripts/ScriptableObejcts/CharacterProperties.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Maze.ScriptableObejcts
+{
+    [CreateAssetMenu(fileName = "new Character Properties", menuName = "Character/ Properties", order = 0)]
+    public class CharacterProperties : ScriptableObject
+    {
+        [Header("Movement Properties")] 
+        public float Speed;
+
+        [Header("Rotation Properties")] 
+        public float RotationSpeed;
+    }
+}
