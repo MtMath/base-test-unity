97030c341716a1222488afbf1cc3b97cb741abac
Matheus Costa <matheus.design12@gmail.com>
2022-07-24 15:43:05 -0300

    Character Class

---
+++ Assets/Scripts/Character/Character.cs
@@ -0,0 +1,13 @@
+﻿using Maze.ScriptableObejcts;
+using UnityEngine;
+
+namespace Maze.Character
+{
+    public abstract class Character : MonoBehaviour
+    {
+        public CharacterProperties CharacterProperties;
+        
+        public abstract Vector3 GetVelocity();
+        public abstract Vector3 GetMovementVelocity();
+    }
+}
