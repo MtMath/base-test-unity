bd5369b140d9512d5d03b7d4c77cb9bf7a3cbe78
Matheus Costa <matheus.design12@gmail.com>
2022-07-24 16:30:50 -0300

    Update Character.cs

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -1,33 +1,11 @@
 ﻿using Maze.ScriptableObejcts;
 using UnityEngine;
-using UnityEngine.AI;
 
 namespace Maze.Character
 {
     public abstract class Character : MonoBehaviour
     {
         public CharacterProperties CharacterProperties;
-        public NavMeshAgent nav { get; private set; }
-
-        private void Awake()
-        {
-            Initialize();
-        }
-
-        private void Initialize()
-        {
-            nav = GetComponent<NavMeshAgent>();
-
-        }
-        
-        public void SetVelocity(Vector3 velocity)
-        {
-            if(velocity == Vector3.zero) return;
-            Vector3 destination = velocity;
-            
-            nav.Move(destination * Time.deltaTime);
-        }
-
         public abstract Vector3 GetVelocity();
         public abstract Vector3 GetMovementVelocity();
     }
